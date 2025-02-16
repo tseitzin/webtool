2f297dccac25ec50b8c7fab7507b03269a600d6d
tseitzin <tseitzinger@gmail.com>
2025-02-16 18:44:33 -0500

    Added decimal places formatting for items under .00

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,11 +1,28 @@
 export const formatNumber = (num: number) => {
-    return new Intl.NumberFormat('en-US').format(num)
+  return new Intl.NumberFormat('en-US').format(num)
+}
+
+export const formatCurrency = (num: number) => {
+  // For very small numbers (less than 0.01), show more decimal places
+  if (Math.abs(num) < .01) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 8,
+      maximumFractionDigits: 8
+    }).format(num)
   }
   
-  export const formatCurrency = (num: number) => {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(num)
-  }
-  
-  export const formatPercent = (num: number) => {
-    return new Intl.NumberFormat('en-US', { style: 'percent', minimumFractionDigits: 2 }).format(num / 100)
-  }
+  // Default currency formatting for normal numbers
+  return new Intl.NumberFormat('en-US', { 
+    style: 'currency', 
+    currency: 'USD' 
+  }).format(num)
+}
+
+export const formatPercent = (num: number) => {
+  return new Intl.NumberFormat('en-US', { 
+    style: 'percent', 
+    minimumFractionDigits: 2 
+  }).format(num / 100)
+}
